493c62d9db19ac8831fbeed93eea856d7137726d
Johnny Chen <j_chen187@yahoo.com>
2015-12-06 16:08:58 -0500

    Starting the training project

---
+++ Neural Network Training/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Neural Network Training
+//
+//  Created by Johnny Chen on 12/6/15.
+//  Copyright (c) 2015 JohnnyChen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
