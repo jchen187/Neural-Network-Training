5ca20e5de0a0e1476042477bec20b95a3d315dc2
Johnny Chen <j_chen187@yahoo.com>
2015-12-06 16:43:43 -0500

    It can now read the first line from the initial neural network

--- Neural Network Training/main.cpp
+++ Neural Network Training/main.cpp
@@ -7,9 +7,51 @@
 //
 
 #include <iostream>
+#include <fstream> //reading from file
+
+int inputNodes, hiddenNodes, outputNodes;
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    string file1, file2, file3;
+    cout << "Please enter the file containing the initial neural network\n";
+    //read the file
+    cin >> file1;
+    
+    ifstream myFile;
+    myFile.open(file1);
+    if (myFile.is_open()){
+        //Put contents of file into array
+        for (int i = 0; i <= 3; i++){
+            if (i == 0)
+                myFile >> inputNodes;
+            if (i == 1)
+                myFile >> hiddenNodes;
+            if (i == 2){
+                myFile >> outputNodes;
+            }
+        }
+        myFile.close();
+    }
+    else
+        cout << "Unable to open file.\n";
+    
+    cout << "Please enter the file contraining the training set.\n";
+    cout << "Where would you like to output the results to?\n";
     return 0;
 }
+
+/*
+ 1st line has 3 integers
+ - number of input nodes
+ - number of hidden nodes 
+ - number of output nodes
+ 
+ Next Nh lines each has Ni + 1 weights entering the 1st hidden node, then 2nd ... until Nh
+ There is a +1 because a bias weight which is always the first weight
+ Weights are floating point numbers
+ 
+ Next No has Nh + 1 weights
+*/
